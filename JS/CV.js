6a0a14560b58fd9a1a1d26ec25e6c99a03ef6c9b
Lucho oviedo <luchoviedo28@gmail.com>
2023-02-21 09:00:53 -0300

    Add files via upload

---
+++ JS/CV.js
@@ -0,0 +1,24 @@
+function twitter() {
+    let text = "Desea ir a mi perfil de twitter?";
+    if (confirm(text) == true) {
+        window.location.href = "http://www.twitter.com";
+    }
+}
+function facebook() {
+    let text = "Desea ir a mi perfil de facebook?";
+    if (confirm(text) == true) {
+        window.location.href = "http://www.facebook.com";
+    }
+}
+function linkedin() {
+    let text = "Desea ir a mi perfil de linkedin?";
+    if (confirm(text) == true) {
+        window.location.href = "http://www.linkedin.com";
+    }
+}
+function instagram() {
+    let text = "Desea ir a mi perfil de instagram?";
+    if (confirm(text) == true) {
+        window.location.href = "http://www.instagram.com";
+    }
+}
